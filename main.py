57e786cfc932533bd5f1dbaea0bee4ef859249e5
123-dot-com <cuber.akshat@gmail.com>
2026-07-03 19:17:22 +0530

    Removed testing lines

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import gmail_reader as gr
 import sheet_writer as sw
 
 credentials = creds.create_creds()
-#order = gr.main(credentials)
-order = 'me'
+order = gr.main(credentials)
 sw.main(credentials, order)
 
 print ('done')
